c1b5cda23d8621081fc12de077237a75e82435f8
jyunya-takahashi <67084292+jyunya-takahashi@users.noreply.github.com>
2021-07-28 23:47:40 +0900

    変数宣言

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import "./styles.css";
+// 変数宣言
+// 旧[ver]は上書き＆再宣言が可能ー＞使用しない
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// [let]は上書き可能、再宣言不可
+let val2 = "let変数";
+console.log(val2);
+val2 = "let再宣言";
+console.log(val2);
+
+// [const]上書き＆再宣言不可
+const val3 = "const変数";
+console.log(val3);
+
+// [const]オブジェクトの場合、内部の値(プロパティ)は変更可能
+const val4 = {
+  name: "test",
+  age: 30
+};
+val4.name = "test2";
+// 新規プロパティ追加
+val4.address = "Tokyo";
+console.log(val4);
+
+// [const]配列の場合、内部の値(プロパティ)は変更可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+// 配列要素追加
+val5.push("monkey");
+console.log(val5);
